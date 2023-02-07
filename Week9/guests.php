b275248d8adedd2035d37171ff4ac1de064781c8
Angelo Villaflor <anvillaflor@student.apc.edu.ph>
2023-02-07 23:44:45 +0800

    HTML Form & PHP to mySQL [8]

--- Week9/guests.php
+++ Week9/guests.php
@@ -21,7 +21,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "ID: ". $row["id"]. "<br>" . "Full Name: ". $row["firstname"]. "<br>". "Email: " . $row["email"] . "<br>";
+        echo "ID: ". $row["id"]. "<br>" . "Full Name: ". $row["fullname"]. "<br>". "Email: " . $row["email"] . "<br>";
     }
 } else {
     echo "0 results";
